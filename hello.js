79937477f9b7982faafdda0c59f58acc1491f982
nky214 <nky214@gmail.com>
2018-10-19 23:18:56 -0400

    test is going on

---
+++ hello.js
@@ -0,0 +1,41 @@
+var https = require("https");
+
+
+
+var a=3;
+var b=4;
+
+a+b;
+
+console.log(a+b);
+
+console.log(process.env);
+//console.log(process.arch);
+//console.log(process.memoryUsage);
+
+function sayHello(){
+    console.log('hello world SetTimeOut');
+}
+
+setTimeout(sayHello, 5*1000);
+
+console.time('Summm');
+var sum = 0;
+var j = 0;
+for(var i = 0; i<1000000000; i++ ){
+    
+    j = j+i;
+}
+
+console.timeEnd('Summm');
+
+
+process.on('uncaughtException', function(code){
+    console.log('uncaughtException:');
+})
+
+sayException();
+
+
+
+
